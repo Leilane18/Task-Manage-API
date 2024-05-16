51b6ba99dece93e2bd95c0b7b8ac395cbbe2a5b5
Leilane Henrique <leilanehenrique18@gmail.com>
2024-05-16 16:21:44 -0300

    add user created service

--- src/controllers/userControllers.ts
+++ src/controllers/userControllers.ts
@@ -1,40 +1,45 @@
 import { Request, NextFunction, Response } from "express";
 import { string, z } from "zod";
+import { userServices } from "../services/userServices";
 
 export const userControllers = {
   async create(req: Request, res: Response, next: NextFunction) {
     try {
-      const userSchema = z.object({
-        name: z
-          .string({
-            required_error: "name is required!",
-            invalid_type_error: "name must be a string!",
+      const userSchema = z
+        .object({
+          name: z
+            .string({
+              required_error: "name is required!",
+              invalid_type_error: "name must be a string!",
+            })
+            .min(3, "name must have at least 3 characters!")
+            .max(255, "max name length exceeded!"),
+
+          email: string({
+            required_error: "email is required!",
+            invalid_type_error: "email must be a string!",
           })
-          .min(3, "name must have at least 3 characters!")
-          .max(255, "max name length exceeded!"),
+            .email("email poorly formatted!")
+            .max(255, "max email length exceeded!"),
 
-        email: string({
-          required_error: "email is required!",
-          invalid_type_error: "email must be a string!",
+          password: string({
+            required_error: "password is required!",
+            invalid_type_error: "password must be a string!",
+          })
+            .min(7, "password must have at least 7 characters!")
+            .max(255, "max password length exceeded!")
+            .regex(/^(?=.*[A-Z])(?=.*\d)(?=.*[^A-Za-z\d]).{7,}$/, {
+              message:
+                "the password must contain at least one capital letter, one number and one special character!",
+            }),
         })
-          .email("email poorly formatted!")
-          .max(255, "max email length exceeded!"),
-
-        password: string({
-          required_error: "password is required!",
-          invalid_type_error: "password must be a string!",
-        })
-          .min(7, "password must have at least 7 characters!")
-          .max(255, "max password length exceeded!")
-          .regex(/^(?=.*[A-Z])(?=.*\d)(?=.*[^A-Za-z\d]).{7,}$/, {
-            message:
-              "the password must contain at least one capital letter, one number and one special character!",
-          }),
-      })
-      .strict();
+        .strict();
 
       const { name, email, password } = userSchema.parse(req.body);
-      return res.status(201).json({ message: "User create!"});
+
+      const userCreated = await userServices.create({ name, email, password }, );
+
+      return res.status(201).json({ message: "User create!", userCreated });
     } catch (error) {
       return next(error);
     }
--- src/services/userServices.ts
+++ src/services/userServices.ts
@@ -1,1 +1,34 @@
+import { hash } from "bcrypt";
+import { randomUUID } from "node:crypto";
 
+type UserData = {
+  name: string;
+  email: string;
+  password: string;
+};
+
+type Repository = {
+    createUser(data: UserData) : Promise<UserData | undefined>;
+};
+
+export const userServices = {
+  async create(data: UserData, repository: Repository) {
+    try {
+      const { name, email, password } = data;
+
+      const passwordHash = await hash(password, 10);
+
+      const user = {
+        id: randomUUID(),
+        name,
+        email,
+        password: passwordHash,
+      };
+
+      const userCreated = await repository.createUser(user);
+
+    } catch (error) {
+      throw error;
+    }
+  },
+};
