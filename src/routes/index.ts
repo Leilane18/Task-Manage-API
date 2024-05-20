224ea1f1cb883e94bb3272d74571e812c4b7b53e
Leilane Henrique <leilanehenrique18@gmail.com>
2024-05-20 16:15:55 -0300

    add auth functions

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,8 @@
 import { Router } from "express";
 import { userRoutes } from "./user.routes";
+import { authRoutes } from "./auth.routes";
 
 export const router = Router();
 
 router.use(userRoutes);
+router.use(authRoutes);
